b5ab780bede4ac4e39b5d0eb077772aaa361fd9c
samueljmurray <samueljmurray@gmail.com>
2015-03-12 12:19:32 +0000

    initial commit

---
+++ app/SC-app-header.module.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name SC-app-header
+ * @description
+ *
+ * Provides the app with the ability to display header content and features
+ */
+angular
+  .module('SC-app-header');
